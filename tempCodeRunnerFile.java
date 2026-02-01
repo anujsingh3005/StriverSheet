b655ce41c49ea245fecf7efe41f18cfc00213964
Mac <anujsingh30005@gmail.com>
2026-02-01 16:08:30 +0530

    error in dir

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+public static void main(String[] args){
+        int [] nums = {-4, -3, 0, 1, -8};
+        int number = largestElement(nums);
+        System.out.println(number);
+    }
+
+    public static int largestElement(int[] nums) {
+    Arrays.sort(nums);
+    int len = nums.length;
+    return nums[len-1];
+    }
