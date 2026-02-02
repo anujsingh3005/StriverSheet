948b5391e88d8f38bc08c1cbd00074866432a8ee
Mac <anujsingh30005@gmail.com>
2026-02-02 21:35:45 +0530

    optimal

--- FindMissingNo.java
+++ FindMissingNo.java
@@ -7,11 +7,21 @@ public class FindMissingNo {
         System.out.println(n);
     }
     public static int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i =0 ; i<nums.length; i++){
-            if ( i != nums[i]) return i;
-            if(i == nums.length-1) return i+1; 
+        // Arrays.sort(nums);
+        // for(int i =0 ; i<nums.length; i++){
+        //     if ( i != nums[i]) return i;
+        //     if(i == nums.length-1) return i+1; 
+        // }
+        // return -1;
+
+        int n = nums.length;
+        //Gauss' Formula
+        int expSum = n * (n+1) / 2;
+        int actualSum = 0;
+
+        for(int i=0; i<n; i++){
+            actualSum += nums[i];
         }
-        return -1;
+        return expSum - actualSum;
     }
 }
