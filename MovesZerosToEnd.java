27690e4223c31bbc4f3c24ac78f5a8c0e748c569
Mac <anujsingh30005@gmail.com>
2026-02-02 20:18:04 +0530

    optimal method

--- MovesZerosToEnd.java
+++ MovesZerosToEnd.java
@@ -4,20 +4,38 @@ public class MovesZerosToEnd {
         moveZeroes(nums);
     }
     public static void moveZeroes(int[] nums) {
-        int k=0;
-        int counter = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i] == 0) k++;
-            else{
-                nums[counter] = nums[i];
-            counter++;
+        // int k=0;
+        // int counter = 0;
+        // for(int i = 0; i<nums.length; i++){
+        //     if(nums[i] == 0) k++;
+        //     else{
+        //         nums[counter] = nums[i];
+        //     counter++;
+        //     }
+        // }
+        // int j = nums.length - 1;
+        // while(k > 0){
+        //     nums[j] = 0;
+        //     k--;
+        //     j--;
+        // }
+
+        int j =-1;
+        for(int i=0; i< nums.length; i++){
+            if(nums[i] == 0) {
+                j = i;
+                break;
             }
         }
-        int j = nums.length - 1;
-        while(k > 0){
-            nums[j] = 0;
-            k--;
-            j--;
+        if(j == -1) return;
+        
+        for(int i=j+1; i<nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+            }
         }
 
         for(int i =0; i<nums.length; i++){
