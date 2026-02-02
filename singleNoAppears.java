100fd91e4aa0ab4437599b9d627dd100e6f04002
Mac <anujsingh30005@gmail.com>
2026-02-02 23:01:02 +0530

    Occurence

---
+++ singleNoAppears.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class singleNoAppears {
+    public static void main(String[] args){
+        int [] nums = {5};
+        int number = singleNumber(nums);
+        System.out.println(number);
+    }
+
+    public static int singleNumber(int[] nums) {
+        int xor = nums[0];
+        for(int i=1; i<nums.length; i++){
+            xor ^= nums[i];
+        }
+        return xor;
+    }
+}
