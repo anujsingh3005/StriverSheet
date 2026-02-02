fdd02778eb9b6749b71b26ac3459ccf925be3e5e
Mac <anujsingh30005@gmail.com>
2026-02-02 22:50:59 +0530

    Occurence problem

---
+++ findMaxOccurence.java
@@ -0,0 +1,18 @@
+public class findMaxOccurence {
+    public static void main(String[] args) {
+        int [] nums = {1, 0, 1, 1, 1, 0, 1, 1, 1, 1};
+        int k = findMaxConsecutiveOnes(nums);
+        System.out.println(k);
+    }
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count=0, newcount = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 1) {
+                newcount++;
+                if(newcount > count) count = newcount;
+            }
+            else newcount = 0;
+        }
+        return count;
+    }
+}
