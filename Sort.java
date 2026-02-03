0403ae03a0e8bfb6a3efadd6add784dc4a82e03e
Mac <anujsingh30005@gmail.com>
2026-02-04 00:29:02 +0530

    Optimal Approach, Dutch Flag Algo

---
+++ Sort.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 0, 0, 2, 1};  
+        sortZeroOneTwo(nums);
+    }
+    // public static void sortZeroOneTwo(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for(int i=0; i<nums.length; i++){
+    //         System.out.print(nums[i] + " ");
+    //     }
+    // }
+
+    //Dutch national flag algo
+    public static void sortZeroOneTwo(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }else if( nums[mid] == 1) mid ++;
+            else{
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+        }
+
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
