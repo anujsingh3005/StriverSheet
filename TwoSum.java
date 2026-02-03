a7f83ced7d11b5b5b23bfd4d7a4d9bc6dda17c42
Mac <anujsingh30005@gmail.com>
2026-02-04 00:15:50 +0530

    Optimal 2Sum

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
     public static void main(String[] args) {
         int [] nums = {1, 3, 5, -7, 6, -3};
@@ -5,11 +9,21 @@ public class TwoSum {
         int[] ans = twoSum(nums,target);
         System.out.print( ans[0] + " " + ans[1]);
     }
+    // public static int[] twoSum(int[] nums, int target) {
+    //     for(int i = 0; i<nums.length-1; i++){
+    //         for(int j = i + 1; j<nums.length; j++){
+    //             if(nums[i] + nums[j] == target) return new int[]{i, j};
+    //         }
+    //     }
+    //     return new int[]{-1,-1};
+    // }
     public static int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i<nums.length-1; i++){
-            for(int j = i + 1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target) return new int[]{i, j};
-            }
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int complemet = target - nums[i];
+            if(map.containsKey(complemet)) return new int[]{map.get(complemet), i};
+            map.put(nums[i], i);
         }
         return new int[]{-1,-1};
     }
