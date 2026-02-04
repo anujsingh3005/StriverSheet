176e6fc9a9fc3391aa5d3d585d4046f64f4e7a28
Mac <anujsingh30005@gmail.com>
2026-02-05 00:56:16 +0530

    Optimal buy and sell stock

--- buySellStocks.java
+++ buySellStocks.java
@@ -1,22 +1,19 @@
 public class buySellStocks {
     public static void main(String[] args) {
-        int[] nums = {10, 7, 5, 8, 11, 9};
+        int[] nums = {5, 4, 3, 2, 1};
         int n = nums.length;
         int k = stockBuySell(nums, n);
         System.out.println(k);
     }
     public static int stockBuySell(int[] nums, int n) {
-        int maxsub = 0;
-        int sub=0;
-        for(int i=0; i<n-1;i++){
-            for(int j=i+1; j<n; j++){
-                sub = nums[j] - nums[i];
-            if(sub < 0){
-                break;
-            } 
-            maxsub = Math.max(maxsub, sub);
-            }
+        int maxpro = 0;
+        int min=Integer.MAX_VALUE;
+
+        for(int items : nums){
+            if(items < min) min = items;
+            else maxpro = Math.max(maxpro, items - min);
         }
-       return maxsub;
+        
+       return maxpro;
     }
 }
