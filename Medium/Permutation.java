eb6ec5747ecb5f166c7a71cdc0a7a1b554d1fca6
Mac <anujsingh30005@gmail.com>
2026-02-06 20:47:57 +0530

    Permutations in java

---
+++ Medium/Permutation.java
@@ -0,0 +1,49 @@
+public class Permutation {
+    public static void main(String[] args) {
+        int [] nums = {2,3,1};
+        nextPermutation(nums);
+
+        for(int items: nums){
+            System.out.print(items + " ");
+        }
+
+    }
+    public static void nextPermutation(int[] nums) {
+       int index = -1;
+       int n = nums.length;
+       
+       for(int i = n - 2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                index=i;
+                break;
+            } 
+       }
+
+       if(index == -1){
+        reverse(nums, 0, n-1);
+        return;
+       } 
+        
+       for(int i = n-1; i>index; i--){
+            if(nums[i] > nums[index]){
+                swap(nums, i, index); 
+                break;
+            } 
+       }
+       reverse(nums, index+1, n-1);
+
+    }
+    public static void reverse(int [] nums, int start, int end){
+        while(start<end){
+            swap(nums, start, end);
+                start++;
+                end--;
+        }
+    }
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
