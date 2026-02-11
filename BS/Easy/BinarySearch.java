8a769332bb555823507ccfc3d0575f7dfbc4ed19
Mac <anujsingh30005@gmail.com>
2026-02-11 19:45:39 +0530

    Did basic binary search

---
+++ BS/Easy/BinarySearch.java
@@ -0,0 +1,21 @@
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while(low <= high){
+            int mid = ((high + low)/2);
+            if( target == nums[mid] ) return mid;
+            else if( target > nums[mid]) low = mid+1;
+            else high = mid-1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int [] nums = {-1,0,3,5,9,12};
+        int target = 9;
+
+        int k = search(nums, target);
+        System.out.println(k);
+    }
+}
