7e66354880efb822e7a0da8d4272a9d9514ef116
Mac <anujsingh30005@gmail.com>
2026-02-12 23:19:45 +0530

    this is the most optimal answer, of elements in an sorted array

--- BS/Easy/ElementInSortedArray.java
+++ BS/Easy/ElementInSortedArray.java
@@ -1,41 +1,46 @@
 public class ElementInSortedArray {
     public static int[] searchRange(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-        int mid = -1;
-        
-        while(low <= high){
-            mid = (high + low)/2;
-            if(target > nums[mid]) low = mid+1;
-            else if(target < nums[mid]) high = mid-1;
-            else break;
-        }
 
-        int s = starting(nums, mid, target);
-        int e = ending(nums, mid, target);
+        int s = starting(nums, target);
+        int e = ending(nums, target);
 
         return new int[]{s,e};
     }
 
 
-    public static int starting(int[] nums, int occ, int target){
+    public static int starting(int[] nums, int target){
         int ans = -1;
         
-        if(occ == -1) return -1;
-        else if(occ == 0 && nums[occ] == target) return 0;
-        
-        for(int i=occ; i>=0; i--){
-            if(nums[i] == target) ans=i;
+        int low=0 , high=nums.length-1;
+
+        while(low<=high){
+            int mid = (low+high) / 2;
+
+            if(target == nums[mid]){
+                ans= mid;
+                high=mid-1;
+            }else if(target > nums[mid]) low = mid+1;
+            else high = mid - 1;
         }
+        
         return ans;
     }
 
-    public static int ending(int[] nums, int occ, int target){
+    public static int ending(int[] nums,  int target){
         int ans = -1;
-        if(occ == -1) return -1;
-        else if(occ == (nums.length - 1) && nums[occ] == target) return occ;
-        for(int i=occ; i<nums.length; i++){
-            if(nums[i] == target) ans=i;
+        
+        int low=0, high=nums.length-1;
+
+        while(low<=high){
+            int mid = (low+high) / 2;
+
+            if(target == nums[mid]){
+                ans= mid;
+                low=mid+1;
+            }else if(target > nums[mid]) low = mid+1;
+            else high = mid - 1;
         }
+
         return ans;
     }
 
