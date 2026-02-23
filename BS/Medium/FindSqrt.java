40e244cfc6f48e579b15f905f59b3db82a4ecdc6
Mac <anujsingh30005@gmail.com>
2026-02-23 21:50:58 +0530

    I did my self

---
+++ BS/Medium/FindSqrt.java
@@ -0,0 +1,25 @@
+public class FindSqrt {
+    public static void main(String[] args) {
+        int nums = 28;
+
+        int result = floorSqrt(nums);
+
+        System.out.println(result);
+    }
+
+    public static int floorSqrt(int n) {
+      int ans = -1, low=1, high=n;
+
+      while(low<=high){
+        int mid = low + (high-low)/2;
+        int prod = mid*mid;
+        if(prod <= n){
+            ans = mid;
+            low=mid+1;
+        }
+        else high = mid -1;
+      }
+
+      return ans;
+    }
+}
