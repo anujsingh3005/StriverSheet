964835ed1319705226990d940fd9daf8d56ca7ef
Mac <anujsingh30005@gmail.com>
2026-02-23 21:54:20 +0530

    Most optimal ans

--- BS/Medium/FindSqrt.java
+++ BS/Medium/FindSqrt.java
@@ -13,13 +13,13 @@ public class FindSqrt {
        if (n < 2) return n;
 
       while(low<=high){
-        int mid = low + (high-low)/2;
-        int prod = mid*mid;
-        if(prod <= n){
-            ans = mid;
-            low=mid+1;
+        long mid = low + (high-low)/2;
+        
+        if(mid*mid <= n){
+            ans = (int) mid;
+            low= (int)mid+1;
         }
-        else high = mid -1;
+        else high = (int)mid -1;
       }
 
       return ans;
