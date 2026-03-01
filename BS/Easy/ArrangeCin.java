5e1999de4aa03cd3eb3d990fda36eb99d3759216
Mac <anujsingh30005@gmail.com>
2026-03-02 02:33:49 +0530

    optimal solution

--- BS/Easy/ArrangeCin.java
+++ BS/Easy/ArrangeCin.java
@@ -1,25 +1,15 @@
 public class ArrangeCin {
     public static int arrangeCoins(int n) {
-        // long low = 1, high=(n/2)+1;
-        // while(low<=high){
-        //     long mid = low + (high-low)/2;
-        //     long req = (mid * (mid + 1))/2;
-        //     if(req == (long)n) return (int) mid;
-        //     else if(req<=(long)n) low=mid+1;
-        //     else high=mid-1;
-        // }
-        // return (int)high;
-        int count = 0; 
-        int i = 1; 
-        
-        
-        while(n >= i){
-                n -= i;   
-                count++;  
-                i++;      
+        long low = 1, high=(n/2)+1;
+        while(low<=high){
+            long mid = low + (high-low)/2;
+            long req = (mid * (mid + 1))/2;
+            if(req == (long)n) return (int) mid;
+            else if(req<=(long)n) low=mid+1;
+            else high=mid-1;
         }
-       
-       return count;
+        return (int)high;
+
     }
     public static void main(String[] args) {
         int n = 6;
